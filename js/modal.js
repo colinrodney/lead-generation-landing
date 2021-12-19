05c53bb902942ff15007a3388d46f7dca412d74b
C.Renauld Rodney <c.renauld.rodney@gmail.com>
2021-12-19 09:20:39 -0600

    add notes on how to handle button click

--- js/modal.js
+++ js/modal.js
@@ -5,9 +5,12 @@ let html = document.getElementById("html-main");
 console.log(html);
 
 // change HTML background tint to darkgray color rgb(54,54,54)
-let testStyle;
-testStyle.background =
+html.style.background =
   "linear - gradient(rgba(54,54,54,.45),rgba(54,54,54,.45))";
+
+// consider creating a class w/ these styles in CSS file and add className to HTML element when main cta button is clicked"
+
+// console.log(html.style.background);
 
 // button click event
 let button = document.getElementById("main-cta");
