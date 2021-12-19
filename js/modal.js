98427da052cd3cbb672b96836183c48aa50278a8
C.Renauld Rodney <c.renauld.rodney@gmail.com>
2021-12-19 08:59:45 -0600

    add event listener to call-to-action button

--- js/modal.js
+++ js/modal.js
@@ -3,3 +3,17 @@ console.warn("connected");
 // DOM SETUP
 let html = document.querySelector("html");
 console.log(html);
+
+// change HTML background tint to darkgray color rgb(54,54,54)
+// html.style.background =
+//   "linear - gradient(rgba(54,54,54,.45),rgba(54,54,54,.45))";
+
+// button click event
+let button = document.getElementsByClassName("main-content__cta");
+console.log(button);
+
+button[0].addEventListener("click", myFunction);
+
+function myFunction() {
+  console.log("button clicked");
+}
